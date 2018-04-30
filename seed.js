76956e652f9947d5d879b6e014459082c48589aa
Teresa <li.teresa.y@gmail.com>
2018-04-30 17:25:05 -0400

    second

--- seed.js
+++ seed.js
@@ -1,17 +1,29 @@
 const db = require('./models');
 
+
 // db.sync({force: true}).then(db.close).catch(db.close);
 
 // const db = require('./models')
-
+let sar = [];
+sar.push(new db.Vegetable({
+  name:
+  color:
+  planted_on:
+}))
 db.sync({force: true})
   .then(() => {
     console.log('Database synced!')
-    db.close() // only if using a version of node without `finally`
+    new db.
+    // db.close(); // only if using a version of node without `finally`
   })
   .catch(err => {
     console.log('Disaster! Something went wrong! ')
     console.log(err)
-    db.close() // only if using a version of node without `finally`
+    // db.close() // only if using a version of node without `finally`
   })
+  .finally(() => {
+    db.close();
+  })
+
+
 
